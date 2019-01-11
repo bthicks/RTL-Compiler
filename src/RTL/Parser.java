b2fc9c6c8f92290075d1c354142be0c345650b06
Eitan Simler <eitan.simler@gmail.com>
2019-01-11 14:01:28 -0800

    Started working in python

---
+++ src/RTL/Parser.java
@@ -0,0 +1,17 @@
+public class Parser {
+    public static void parse(String fileName) {
+        List<List<String>> result = new ArrayList()<>;
+
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+        }
+    }
+}
