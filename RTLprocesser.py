f5aac35d9a7aebc980e8ef0ead9b1ab80a8ae410
Eitan Simler <eitan.simler@gmail.com>
2019-01-30 12:04:48 -0800

    Instructions are now passed along. Also cmp_insns are born.

--- RTLprocesser.py
+++ RTLprocesser.py
@@ -32,11 +32,12 @@ class RTLprocesser:
         for instruction in instructions:
             function = functions.get(instruction[0])
 
+            new_insn = {}
+
             if function is None:
-                print(instruction[0])
-                new_insn = RTLprocesser._process_other(instruction)
+                RTLprocesser._process_other(instruction, new_insn)
             else:
-                new_insn = function(instruction)
+                function(instruction, new_insn)
 
             if new_insn is not None:
                 result.append(new_insn)
@@ -44,58 +45,48 @@ class RTLprocesser:
         return result
 
     @staticmethod
-    def _preprocess(instruction):
+    def _preprocess(instruction, new_insn):
         """Preprocess the instruction to include common fields.
 
         Args:
             instruction (List[str]): An RTL instruction.
-
-        Returns:
-            dict: The preprocessed instruction
         """
         # ["note", "insn", "jump_insn", "call_insn", "code_label", "barrier"]
-        new_ins = {
+        new_insn.update({
             "type": instruction[0],
             "uid": int(instruction[1]),
             "prev": int(instruction[2]),
             "next": int(instruction[3]),
-        }
+        })
 
         if len(instruction) >= 5 and instruction[4].isdigit():
-            new_ins["block"] = int(instruction[4])
-
-        return new_ins
+            new_insn["block"] = int(instruction[4])
 
     @staticmethod
-    def _process_note(instruction):
-        new_instruction = RTLprocesser._preprocess(instruction)
+    def _process_note(instruction, new_insn):
+        RTLprocesser._preprocess(instruction, new_insn)
 
         if instruction[-1] != "NOTE_INSN_DELETED":
-            new_instruction["note_type"] = instruction[-1]
-
-            return new_instruction
-        return None
+            new_insn["note_type"] = instruction[-1]
 
     @staticmethod
-    def _process_barrier(instruction):
-        new_instruction = RTLprocesser._preprocess(instruction)
+    def _process_barrier(instruction, new_insn):
+        RTLprocesser._preprocess(instruction, new_insn)
 
-        new_instruction["block"] = -1
-
-        return new_instruction
+        new_insn["block"] = -1
 
     @staticmethod
-    def _process_insn(instruction):
-        new_instruction = RTLprocesser._preprocess(instruction)
+    def _process_insn(instruction, new_insn):
+        RTLprocesser._preprocess(instruction, new_insn)
 
-        new_instruction["expr"] = {
+        new_insn["expr"] = {
             "type": instruction[5][0], "rest": instruction[5][1:]
         }
 
         match = re.match(r"reg(/\w)*:(?P<type>[A-Z]I)",
                          instruction[5][1][0])
         if match:
-            new_instruction["target"] = {
+            new_insn["target"] = {
                 "value": "r{number}:{type}".format(
                     number=instruction[5][1][1],
                     type=match.group('type')),
@@ -103,12 +94,12 @@ class RTLprocesser:
             }
 
             if instruction[5][-1][0] == "const_int":
-                new_instruction["sources"] = [{
+                new_insn["sources"] = [{
                     "value": instruction[5][2][1],
                     "offset": 0
                 }]
         elif instruction[5][1][0] == "reg:CC":
-            new_instruction["target"] = {
+            new_insn["target"] = {
                 "value": "cc:CC",
                 "offset": 0,
             }
@@ -118,7 +109,7 @@ class RTLprocesser:
                              instruction[5][1][1][0])
             if match:
                 if match.group('type') != 'reg':
-                    new_instruction['expr']['mem'] = {
+                    new_insn['expr']['mem'] = {
                         'type': match.group('type'),
                         'rest': instruction[5][1][1][1:]
                     }
@@ -130,37 +121,38 @@ class RTLprocesser:
                             number=instruction[5][1][1][1][1],
                             type=match.group('type'))
 
-                        key = 'sources' if 'target' in new_instruction else 'target'
+                        key = 'sources' if 'target' in new_insn else 'target'
 
                         if instruction[5][1][1][2][0] == 'const_int':
-                            new_instruction[key] = {
+                            new_insn[key] = {
                                 "value": string,
                                 "offset": int(instruction[5][1][1][2][1])
                             }
                         else:
-                            new_instruction[key] = {
+                            new_insn[key] = {
                                 "value": string,
                                 "offset": 0
                             }
 
                         if key == 'sources':
-                            new_instruction[key] = [new_instruction[key]]
+                            new_insn[key] = [new_insn[key]]
                 else:
-                    new_instruction['target'] = {
-                    "value": RTLprocesser._get_register(instruction[5][1][1]),
-                    "offset": 0
-                }
-            elif RTLprocesser._get_register(instruction[5][1][1]):
-                new_instruction['target'] = {
-                    "value": RTLprocesser._get_register(instruction[5][1][1]),
+                    new_insn['target'] = {
+                        "value": RTLprocesser._get_register(
+                            instruction[5][1][1], new_insn),
+                        "offset": 0
+                    }
+            elif RTLprocesser._get_register(instruction[5][1][1], new_insn):
+                new_insn['target'] = {
+                    "value": RTLprocesser._get_register(instruction[5][1][1], new_insn),
                     "offset": 0
                 }
             else:
-                new_instruction['expr']['mem'] = instruction[5][1][1:]
+                new_insn['expr']['mem'] = instruction[5][1][1:]
 
         if len(instruction[5]) >= 3:
             if instruction[5][2][0] == "unspec:SI":
-                new_instruction['sources'] = [
+                new_insn['sources'] = [
                     {
                         "value": "symbol_ref",
                         "offset": 0,
@@ -173,7 +165,7 @@ class RTLprocesser:
                 match = re.match(r"(?P<type>\w*):[A-Z]{2}",
                                  instruction[5][2][1][0])
                 if match:
-                    new_instruction['expr']['mem'] = {
+                    new_insn['expr']['mem'] = {
                         'type': match.group('type'),
                         'rest': instruction[5][2][1][1:]
                     }
@@ -185,36 +177,38 @@ class RTLprocesser:
                             number=instruction[5][2][1][1][1],
                             type=match.group('type'))
 
-                        key = 'sources' if 'target' in new_instruction else 'target'
+                        key = 'sources' if 'target' in new_insn else 'target'
 
                         if instruction[5][2][1][2][0] == 'const_int':
-                            new_instruction[key] = {
+                            new_insn[key] = {
                                 "value": string,
                                 "offset": int(
                                     instruction[5][2][1][2][1])
                             }
                         else:
-                            new_instruction[key] = {
+                            new_insn[key] = {
                                 "value": string,
                                 "offset": 0
                             }
 
                         if key == 'sources':
-                            new_instruction[key] = [new_instruction[key]]
-                elif re.match(r"mem(/([a-z]|[A-Z]))*:[A-Z]{2}", instruction[5][2][0]):
-                    new_instruction['sources'] = [
+                            new_insn[key] = [new_insn[key]]
+                elif re.match(r"mem(/([a-z]|[A-Z]))*:[A-Z]{2}",
+                              instruction[5][2][0]):
+                    new_insn['sources'] = [
                         {
-                            "value": RTLprocesser._get_register(instruction[5][2][1]),
+                            "value": RTLprocesser._get_register(
+                                instruction[5][2][1], new_insn),
                             "offset": 0,
                         }
                     ]
                 else:
-                    new_instruction['expr']['mem'] = instruction[5][2][1:]
+                    new_insn['expr']['mem'] = instruction[5][2][1:]
             elif re.match(r"([a-z]|[A-Z])+:[A-Z]{2}",
                           instruction[5][2][0]):
 
                 if type(instruction[5][2][1][0]) is list:
-                    new_instruction['unparsed'] = instruction[5][2][1]
+                    new_insn['unparsed'] = instruction[5][2][1]
                 else:
                     match = re.match(r"^reg(/\w)*:(?P<type>[A-Z]I)",
                                      instruction[5][2][1][0])
@@ -223,10 +217,10 @@ class RTLprocesser:
                             number=instruction[5][2][1][1],
                             type=match.group('type'))
 
-                        key = 'sources' if 'target' in new_instruction else 'target'
+                        key = 'sources' if 'target' in new_insn else 'target'
 
                         if instruction[5][2][2][0] == 'const_int':
-                            new_instruction[key] = [
+                            new_insn[key] = [
                                 {
                                     "value": string,
                                     "offset": 0
@@ -243,7 +237,7 @@ class RTLprocesser:
                                 number=instruction[5][2][2][1],
                                 type=match.group('type'))
 
-                            new_instruction[key] = [
+                            new_insn[key] = [
                                 {
                                     "value": string,
                                     "offset": 0
@@ -254,9 +248,10 @@ class RTLprocesser:
                                 },
                             ]
             elif "zero_extend" in instruction[5][2][0]:
-                new_instruction['sources'] = [
+                new_insn['sources'] = [
                     {
-                        "value": "zero_extend([" + RTLprocesser._get_register(instruction[5][2][1][1]) + "])",
+                        "value": "zero_extend([" + RTLprocesser._get_register(
+                            instruction[5][2][1][1], new_insn) + "])",
                         "offset": 0
                     }
                 ]
@@ -268,61 +263,54 @@ class RTLprocesser:
                 number=instruction[5][-1][1],
                 type=match.group('type'))
 
-            if "sources" in new_instruction:
-                new_instruction["sources"] += [{
+            if "sources" in new_insn:
+                new_insn["sources"] += [{
                     "value": string,
                     "offset": 0
                 }]
             else:
-                new_instruction["sources"] = [{
+                new_insn["sources"] = [{
                     "value": string,
                     "offset": 0
                 }]
         elif "subreg" in instruction[5][-1][0]:
-            new_instruction["sources"] = [
+            new_insn["sources"] = [
                 {
-                    "value": RTLprocesser._get_register(instruction[5][-1][1]) + "#" + instruction[5][-1][-1],
+                    "value": RTLprocesser._get_register(
+                        instruction[5][-1][1], new_insn) + "#" + instruction[5][-1][-1],
                     "offset": 0
                 }
             ]
 
         if instruction[-1] != ['nil']:
-            new_instruction["expr_list"] = instruction[-1]
-
-        return new_instruction
+            new_insn["expr_list"] = instruction[-1]
 
     @staticmethod
-    def _process_parallel(instruction):
-        new_instruction = {}
-
-        new_instruction["target"] = {
-            "value": RTLprocesser._get_register(instruction[0][1]),
+    def _process_parallel(instruction, new_insn):
+        new_insn["target"] = {
+            "value": RTLprocesser._get_register(instruction[0][1], new_insn),
             "offset": 0,
         }
 
-        return new_instruction
-
     @staticmethod
-    def _process_call_insn(instruction):
-        new_instruction = RTLprocesser._preprocess(instruction)
+    def _process_call_insn(instruction, new_insn):
+        RTLprocesser._preprocess(instruction, new_insn)
 
         if instruction[5][0] == "parallel":
-            new_instruction.update(**RTLprocesser._process_parallel(instruction[5][1]))
+            RTLprocesser._process_parallel(instruction[5][1], new_insn)
 
-        new_instruction["sources"] = [
+        new_insn["sources"] = [
             {
                 "value": "symbol_ref",
                 "offset": 0
             }
         ]
 
-        return new_instruction
-
     @staticmethod
-    def _process_jump_insn(instruction):
-        new_instruction = RTLprocesser._preprocess(instruction)
+    def _process_jump_insn(instruction, new_insn):
+        RTLprocesser._preprocess(instruction, new_insn)
 
-        new_instruction.update({
+        new_insn.update({
             "target": {
                 "value": instruction[5][1][0],
                 "offset": 0
@@ -332,12 +320,11 @@ class RTLprocesser:
         })
 
         if instruction[5][2][0] == "if_then_else":
-            new_instruction['sources'][0]['value'] = RTLprocesser._process_if_then_else(instruction[5][2])
-
-        return new_instruction
+            new_insn['sources'][0][
+                'value'] = RTLprocesser._process_if_then_else(instruction[5][2], new_insn)
 
     @staticmethod
-    def _process_if_then_else(instruction):
+    def _process_if_then_else(instruction, new_insn):
         operators = {
             "ge": ">=",
             "gt": ">",
@@ -348,9 +335,9 @@ class RTLprocesser:
         }
 
         # Get condition
-        result = ["{", "(", RTLprocesser._get_register(instruction[1][1]),
+        result = ["{", "(", RTLprocesser._get_register(instruction[1][1], new_insn),
                   operators.get(instruction[1][0], ""),
-                  RTLprocesser._get_register(instruction[1][2]), ")?"]
+                  RTLprocesser._get_register(instruction[1][2], new_insn), ")?"]
 
         # Get Then and Else
         if instruction[2][0] == "label_ref":
@@ -365,26 +352,23 @@ class RTLprocesser:
         return ''.join(result)
 
     @staticmethod
-    def _get_register(instruction):
-        match = re.match(r"^reg(/\w)*:(?P<type>[A-Z]I)", instruction[0])
+    def _get_register(instruction, new_insn):
+        match = re.match(r"^reg(/\w)*:[A-Z]I", instruction[0])
 
         if match:
-            return "r{number}:{type}".format(
-                number=instruction[1],
-                type=match.group('type'))
+            return instruction[1]
         elif instruction[0] == "reg:CC":
-            return "cc:CC"
+            new_insn['type'] = 'cmp_insn'
+            return "cc"
         elif instruction[0] == "const_int":
             return instruction[1]
 
         return ""
 
     @staticmethod
-    def _process_code_label(instruction):
-        new_instruction = RTLprocesser._preprocess(instruction)
-
-        return new_instruction
+    def _process_code_label(instruction, new_insn):
+        RTLprocesser._preprocess(instruction, new_insn)
 
     @staticmethod
-    def _process_other(instruction):
-        return instruction
+    def _process_other(instruction, new_insn):
+        pass
