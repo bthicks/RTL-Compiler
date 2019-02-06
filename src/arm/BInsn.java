92fc2eeddad4c178ddbcda59711c331f6853fd08
Eitan Simler <eitan.simler@gmail.com>
2019-02-06 11:25:52 -0800

    Added more string formatting

--- src/arm/BInsn.java
+++ src/arm/BInsn.java
@@ -4,14 +4,14 @@ public class BInsn extends AbstractInsn {
     private final Value label;
     private final String condition;
 
-    public BInsn(Value label, String condition)
-    {
+    public BInsn(Value label, String condition, int uid) {
         this.label = label;
         this.condition = condition;
+        this.uid = uid;
     }
 
     @Override
     public String toARM() {
-        return "\tb" + condition + "\t" + label.toString() + "\n";
+        return formatARM("b" + condition, label.toString());
     }
 }
