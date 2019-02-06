b193a65405cd2fa5a10c6d7e11b841d32b05fb55
Eitan Simler <eitan.simler@gmail.com>
2019-02-06 11:18:03 -0800

    Fixing formatter

--- src/arm/AbstractInsn.java
+++ src/arm/AbstractInsn.java
@@ -7,7 +7,7 @@ public abstract class AbstractInsn implements Insn {
     protected int uid;
 
     protected String formatARM(String instruction, String parameters) {
-        return String.format("\t%4s %15s %10s\n", instruction, parameters, "@ insn " + uid);
+        return String.format("\t%-4s\t%-15s\t%-10s\n", instruction, parameters, "@ insn " + uid);
     }
 
     public abstract String toARM();
